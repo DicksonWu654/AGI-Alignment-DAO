f5647ac456bbba4f74ba8bdfe760792a0bb14e9a
Dickson Wu <33645481+DicksonWu654@users.noreply.github.com>
2023-07-21 21:27:42 +0200

    Adding probably buggy base contract

---
+++ contracts/base_governance.sol
@@ -0,0 +1,121 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+contract AGIAlignmentDAO {
+    address public admin; //Admin for now because I need it for the demo lol - but in the future it'll be the dao that sets the threshold
+    uint public proposalThreshold;
+    uint public agreementThreshold;
+
+    enum VoteType {
+        ADD,
+        REMOVE
+    }
+
+    struct Proposal {
+        string text;
+        bool passed;
+        uint votesFor;
+        uint votesAgainst;
+        VoteType voteType;
+    }
+
+    Proposal[] public constitution;
+    mapping(address => bool) public isHuman;
+    mapping(uint => mapping(address => bool)) public votes;
+
+    constructor(uint _proposalThreshold, uint _agreementThreshold) {
+        admin = msg.sender;
+        proposalThreshold = _proposalThreshold;
+        agreementThreshold = _agreementThreshold;
+    }
+
+    function verifyHuman(address _human) external {
+        // TODO: Integrate with Worldcoin's verification
+        // require(msg.sender == admin, "Only admin can verify humans.");
+        isHuman[_human] = true;
+    }
+
+    function changeThresholds(
+        uint _proposalThreshold,
+        uint _agreementThreshold
+    ) external {
+        require(msg.sender == admin, "Only admin can change thresholds.");
+        proposalThreshold = _proposalThreshold;
+        agreementThreshold = _agreementThreshold;
+    }
+
+    function createProposal(string calldata _text) external {
+        require(
+            isHuman[msg.sender],
+            "Only verified humans can create proposals."
+        );
+        constitution.push(
+            Proposal({
+                text: _text,
+                passed: false,
+                votesFor: 0,
+                votesAgainst: 0,
+                voteType: VoteType.ADD
+            })
+        );
+    }
+
+    function vote(uint _proposal, bool _agree) external {
+        require(isHuman[msg.sender], "Only verified humans can vote.");
+        require(_proposal < constitution.length, "Proposal does not exist.");
+        require(!votes[_proposal][msg.sender], "Already voted.");
+
+        votes[_proposal][msg.sender] = true;
+
+        if (_agree) {
+            constitution[_proposal].votesFor++;
+        } else {
+            constitution[_proposal].votesAgainst++;
+        }
+    }
+
+    function checkProposal(uint _proposal) external {
+        require(_proposal < constitution.length, "Proposal does not exist.");
+
+        // If enough votes for adding the proposal
+        if (
+            constitution[_proposal].voteType == VoteType.ADD &&
+            constitution[_proposal].votesFor > proposalThreshold &&
+            (constitution[_proposal].votesFor * 100) /
+                (constitution[_proposal].votesFor +
+                    constitution[_proposal].votesAgainst) >
+            agreementThreshold
+        ) {
+            constitution[_proposal].passed = true;
+            flipProposal(_proposal);
+        }
+        // If enough votes for removing the proposal
+        else if (
+            constitution[_proposal].voteType == VoteType.REMOVE &&
+            constitution[_proposal].votesFor > proposalThreshold &&
+            (constitution[_proposal].votesFor * 100) /
+                (constitution[_proposal].votesFor +
+                    constitution[_proposal].votesAgainst) >
+            agreementThreshold
+        ) {
+            constitution[_proposal].passed = false;
+            flipProposal(_proposal);
+        }
+    }
+
+    function flipProposal(uint _proposal) internal {
+        require(_proposal < constitution.length, "Proposal does not exist.");
+
+        if (constitution[_proposal].voteType == VoteType.ADD) {
+            constitution[_proposal].voteType = VoteType.REMOVE;
+        } else {
+            constitution[_proposal].voteType = VoteType.ADD;
+        }
+
+        constitution[_proposal].votesFor = 0;
+        constitution[_proposal].votesAgainst = 0;
+    }
+
+    function callConstitution() external view returns (Proposal[] memory) {
+        return constitution;
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,78 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+const POLYGON_ZK_EVM_TESTNET_URL = process.env.POLYGON_ZK_EVM_TESTNET_URL
+const GNOSIS_CHIADO_URL = process.env.GNOSIS_CHIADO_URL
+const LINEA_URL = process.env.LINEA_URL
+
+const POLYGON_ZK_EVM_ETHERSCAN_API_KEY = process.env.POLYGON_ZK_EVM_ETHERSCAN_API_KEY
+const GNOSIS_CHIADO_ETHERSCAN_API_KEY = process.env.GNOSIS_CHIADO_ETHERSCAN_API_KEY
+const LINEA_ETHERSCAN_API_KEY = process.env.LINEA_ETHERSCAN_API_KEY
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+
+// TODO: Add rest of the chains
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: false,
+      },
+    },
+  },
+  networks: {
+    polygon_zk_evm_testnet: {
+      url: `${POLYGON_ZK_EVM_TESTNET_URL}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 1442,
+    },
+    gnosis_chiado: {
+      url: `${GNOSIS_CHIADO_URL}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 10200,
+      gasPrice: 100,
+    },
+    linea: {
+      url: `${LINEA_URL}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 59140,
+    },
+
+  },
+  etherscan: {
+    apiKey: {
+      polygon_zk_evm_testnet: `${POLYGON_ZK_EVM_ETHERSCAN_API_KEY}`,
+      gnosis_chiado: `${GNOSIS_CHIADO_ETHERSCAN_API_KEY}`,
+      linea: `${LINEA_ETHERSCAN_API_KEY}`,
+
+    },
+    customChains: [
+      {
+        network: "polygon_zk_evm_testnet",
+        chainId: 1442,
+        urls: {
+          apiURL: "https://api-testnet-zkevm.polygonscan.com/api",
+          browserURL: "https://testnet-zkevm.polygonscan.com"
+        }
+      },
+      {
+        network: "gnosis_chiado",
+        chainId: 10200,
+        urls: {
+          apiURL: "https://blockscout.com/gnosis/chiado/api",
+        }
+      },
+      {
+        network: "linea",
+        chainId: 59140,
+        urls: {
+          apiURL: "https://api-testnet.lineascan.build/api",
+        }
+      }
+    ]
+  }
 };
